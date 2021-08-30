878218dfd0f453757281271f9ebb5f7d521c8934
Max Silva <maxwellfdasilva@gmail.com>
2021-08-30 11:45:41 -0300

    pequenos ajustes de formatação esquação de mackenzie

--- BROV_Comunicacao.py
+++ BROV_Comunicacao.py
@@ -9,16 +9,18 @@ class CalcularVelocidadeSomAgua():
 
     def equacao_mackenzie(self, temperatura, salinidade, profundidade):
         """
-            A equação acima para a velocidade do som na água do mar em função da temperatura, 
+            A equação para a velocidade do som na água do mar em função da temperatura, 
             salinidade e profundidade é dada pela equação de Mackenzie (1981).
             Faixa de validade: temperatura 2 a 30°C, salinidade 25 a 40 partes por mil, profundidade 0 a 8000 m
+            link: http://resource.npl.co.uk/acoustics/techguides/soundseawater/underlying-phys.html#up_mackenzie
         Args:
             temperatura (float): temperatura em graus Celsius (2 a 30°C)
             salinidade (float): salinidade em partes por mil (25 a 40)
             profundidade (float): profundidade em metros (0 a 8000m)
-        Result
-            velocidade_som(float): Velocidade do som na agua 
+        Returns
+            velocidade_som(float): Velocidade do som na agua             
         """
+
         temperatura = float(temperatura)
         salinidade = float(salinidade)
         profundidade = float(profundidade)
@@ -35,17 +37,18 @@ class CalcularVelocidadeSomAgua():
             raise Exception(
                 'Informe uma profundidade válidaentre 0 a 8000 metros')
         #
+        # c(D,S,T) = 1448.96 + 4.591T - 5.304 x 10⁻²T² + 2.374 x 10⁻⁴T³ + 1.340 (S-35) +
+        #            + 1.630 x 10⁻²D + 1.675 x 10⁻⁷D² - 1.025 x 10⁻²T(S - 35) - 7.139 x 10⁻¹³TD³
         resultado_equacao = 1448.96 + 4.591 * temperatura \
-                - 5.304 * pow(10,-2) * pow(temperatura,2) \
-                + 2.374 * pow(10,-4) * pow(temperatura,3) \
-                + 1.340 * (salinidade-35) \
-                + 1.630 * pow(10,-2) * profundidade \
-                + 1.675 * pow(10,-7) * pow(profundidade,2) \
-                - 1.025 * pow(10,-2) * temperatura * (salinidade - 35) \
-                - 7.139 * pow(10,-13) * temperatura * pow(profundidade,3)
+                            - 5.304 * pow(10,-2) * pow(temperatura,2) \
+                            + 2.374 * pow(10,-4) * pow(temperatura,3) \
+                            + 1.340 * (salinidade-35) \
+                            + 1.630 * pow(10,-2) * profundidade \
+                            + 1.675 * pow(10,-7) * pow(profundidade,2) \
+                            - 1.025 * pow(10,-2) * temperatura * (salinidade - 35) \
+                            - 7.139 * pow(10,-13) * temperatura * pow(profundidade,3)
         #
         return resultado_equacao
-
 
 # if __name__ =="__main__":
 #     calcula = CalcularVelocidade()
